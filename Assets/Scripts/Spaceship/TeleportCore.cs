acc667f9864c5f4263f7ef946cc970a642d3a871
galhajaj <galhajaj@gmail.com>
2017-02-10 14:20:18 +0200

    add spaceship data + teleport device with cooldown & many cores

---
+++ Assets/Scripts/Spaceship/TeleportCore.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportCore : MonoBehaviour 
+{
+    private float _cooldownDuration = 0.0F;
+
+    // =================================================================================================
+    void Start()
+    {
+
+    }
+    // =================================================================================================
+    void Update()
+    {
+        if (_cooldownDuration > 0.0F)
+            _cooldownDuration -= Time.deltaTime;
+    }
+    // =================================================================================================
+    public bool IsReady
+    {
+        get { return _cooldownDuration <= 0.0F; }
+    }
+    // =================================================================================================
+    public void AddCooldown(float duration)
+    {
+        _cooldownDuration = duration;
+    }
+    // =================================================================================================
+}
+
---
+++ Assets/Scripts/Spaceship/TeleportDevice.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TeleportDevice : MonoBehaviour 
+{
+    public SpaceshipData Data;
+    public GameObject Core;
+
+    private Queue<TeleportCore> _cores = new Queue<TeleportCore>();
+    // =================================================================================================
+    void Start()
+    {
+        for (int i = 0; i < Data.TeleportCoresNumber; ++i)
+        {
+            addCore();
+        }
+    }
+    // =================================================================================================
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            teleport();
+        }
+    }
+    // =================================================================================================
+    private void addCore()
+    {
+        GameObject coreObject = Instantiate(Core, this.transform) as GameObject;
+        TeleportCore coreScript = coreObject.GetComponent<TeleportCore>();
+        _cores.Enqueue( coreScript );
+    }
+    // =================================================================================================
+    private void teleport()
+    {
+        if (_cores.Peek().IsReady) // check if next core is ready
+        {
+            // add cooldown to used core
+            _cores.Peek().AddCooldown(Data.TeleportCooldownDuration);
+
+            // cycle used core to beginning of queue
+            _cores.Enqueue(_cores.Dequeue());
+
+            // move spaceship to mouse position
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            newPosition = new Vector3(newPosition.x, newPosition.y, 0.0F); // Z to 0
+            transform.position = newPosition;
+        }
+    }
+    // =================================================================================================
+
+}
+
+
