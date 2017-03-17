242a1156e8dc0bda49df6c96c3e288845afc6915
galhajaj <galhajaj@gmail.com>
2017-03-17 13:25:48 +0200

    fix collision stuff + new asteroid (now obstacle) prefab

---
+++ Assets/Scripts/Spaceship/SpaceshipCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceshipCollision : MonoBehaviour 
+{
+	void Start () 
+    {
+		
+	}
+	
+	void Update () 
+    {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D coll) 
+    {
+        // hit obstacle
+        if (coll.gameObject.tag == "Obstacle")
+        {
+            GameControl.instance.gameOver = true;
+            SceneManager.LoadScene ("mainScene");
+        }
+        // hit orb
+        else if (coll.gameObject.tag == "Orb")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
+}
